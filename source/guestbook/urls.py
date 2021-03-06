b4eafe5e09ae252fd24dbac469540d640746549d
Gosha <begoshaaaa@gmail.com>
2021-03-06 12:52:14 +0600

    index.html is added && views.py and urls.py are edited

--- source/guestbook/urls.py
+++ source/guestbook/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from webapp import views as webapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', webapp_views.index_view, name='index')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Guest
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your views here.
+def index_view (request):
+    guests = Guest.objects.all()
+
+    context = {
+        'guests': guests
+    }
+    url = reverse('index')
+    return render(request, 'index.html', context)
